3a14b1d131c3ea330ff3851714b86feaca8e03a1
L Barbee <bluelb84@gmail.com>
2017-07-18 09:33:48 -0400

    second challenge solution added

--- drillset2.js
+++ drillset2.js
@@ -3,7 +3,27 @@
 function jediName(firstName, lastName){
     let firstHalf = firstName.slice(0,2);
     let secondHalf = lastName.slice(0,3);
-    console.log(`${secondHalf}${firstHalf}`);
+    return `${secondHalf}${firstHalf}`;
 }
 
 jediName('Lindsey', 'BarbeeVargas');
+
+// To infinity...
+
+function beyond(num) {
+    if (num === Infinity || num === Number.NEGATIVE_INFINITY) {
+        console.log("And Beyond");
+    }
+    if (isFinite(num) && num > 0){
+        console.log("To infinity");
+    }
+    if (isFinite(num) && num < 0){
+        console.log("To negative infinity");
+    }
+    if (num === 0) {
+        console.log("Staying home");
+    }
+    
+}
+
+beyond(Number.NEGATIVE_INFINITY);
